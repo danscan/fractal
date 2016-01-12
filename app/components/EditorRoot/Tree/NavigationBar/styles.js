e109896579dedeb3bd92d43ace60b9b0b621e736
Dan Scanlon <dscanlonpa@gmail.com>
2016-01-12 09:50:16 -0500

    Removed unused Tree

--- app/components/EditorRoot/Tree/NavigationBar/styles.js
+++
@@ -1,9 +0,0 @@
-import { StyleSheet } from 'react-native';
-
-export default StyleSheet.create({
-  container: {
-    backgroundColor: '#2e3538',
-    // backgroundColor: 'rgba(100,0,0,0.3)',
-    height: 48,
-  },
-});
