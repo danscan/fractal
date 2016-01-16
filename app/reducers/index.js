6c7826bdd27a2e296528e765663b652a7bb11afa
Dan Scanlon <dscanlonpa@gmail.com>
2016-01-16 12:11:50 -0500

    Omit certain action types from undoable history

--- app/reducers/index.js
+++ app/reducers/index.js
@@ -7,11 +7,27 @@ import preview from './preview';
 import selectedElementPath from './selectedElementPath';
 import tree from './tree';
 
+// Filter actions that should be undoable
+import * as actionTypes from '../constants/actionTypes';
+function filterUndoableActions(action) {
+  const actionsToOmitFromHistory = [
+    actionTypes.BEGIN_PREVIEW,
+    actionTypes.END_PREVIEW,
+    actionTypes.PUSH_SELECTED_ELEMENT_PATH,
+    actionTypes.POP_SELECTED_ELEMENT_PATH,
+    actionTypes.REPLACE_SELECTED_ELEMENT_PATH,
+  ];
+
+  return actionsToOmitFromHistory.indexOf(action.type) < 0;
+}
+
 const reducer = undoable(combineReducers({
   editorModalRouteStack,
   preview,
   selectedElementPath,
   tree,
-}));
+}), {
+  filter: filterUndoableActions,
+});
 
 export default reducer;
