2a79d39b443d3e7428b8f642cbee5adba177f763
Dan Scanlon <dscanlonpa@gmail.com>
2016-01-17 15:32:56 -0500

    added inspectorTreePathResolvesProp selector

--- app/selectors/inspectorTreePath.js
+++ app/selectors/inspectorTreePath.js
@@ -1,7 +1,21 @@
 import { createSelector } from 'reselect';
 import { presentState } from './undo';
+import { last } from 'underscore';
 
 export const inspectorTreePath = createSelector(
   presentState,
   (state) => state.inspectorTreePath,
+);
+
+export const inspectorTreePathResolvesProp = createSelector(
+  inspectorTreePath,
+  (inspectorTreePathState) => {
+    const [inspectorTreePathLeafParent, inspectorTreePathLeaf] = last(inspectorTreePathState, 2);
+    const inspectorTreePathLeafParentIsPropsKey = inspectorTreePathLeafParent === 'props';
+    const inspectorTreePathLeafIsNonChildrenPropNameKey = inspectorTreePathLeafParentIsPropsKey
+                                                        && !!inspectorTreePathLeaf
+                                                        && inspectorTreePathLeaf !== 'children';
+
+    return inspectorTreePathLeafIsNonChildrenPropNameKey;
+  },
 );
