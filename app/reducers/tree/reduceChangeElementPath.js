44ff8680ebc2d85145fa800cccb51c93827ce53b
Dan Scanlon <dscanlonpa@gmail.com>
2016-01-26 15:02:08 -0500

    Remove element from old path after moving it to new path

--- app/reducers/tree/reduceChangeElementPath.js
+++ app/reducers/tree/reduceChangeElementPath.js
@@ -1,5 +1,7 @@
 export default function reduceChangeElementPath(state, { elementPath, newElementPath }) {
   const element = state.getIn(elementPath);
+  const treeWithElementAtNewElementPath = state.setIn(newElementPath, element);
 
-  return state.setIn(newElementPath, element);
+  // Return tree with element at new element path, with value at old element path deleted
+  return treeWithElementAtNewElementPath.deleteIn(elementPath);
 }
