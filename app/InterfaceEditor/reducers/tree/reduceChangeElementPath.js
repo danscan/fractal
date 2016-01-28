ad3124fbd48e27749ffda5436a324dbb8f0b00c6
Dan Scanlon <dscanlonpa@gmail.com>
2016-01-28 18:28:11 -0500

    Actually manipulate correct paths (tree paths from element paths)

--- app/InterfaceEditor/reducers/tree/reduceChangeElementPath.js
+++ app/InterfaceEditor/reducers/tree/reduceChangeElementPath.js
@@ -1,6 +1,10 @@
+import treePathByElementPath from '../../utils/treePathByElementPath';
+
 export default function reduceChangeElementPath(state, { elementPath, newElementPath }) {
-  const element = state.getIn(elementPath);
-  const treeWithElementAtNewElementPath = state.setIn(newElementPath, element);
+  const elementTreePath = treePathByElementPath(elementPath);
+  const newElementTreePath = treePathByElementPath(newElementPath);
+  const element = state.getIn(elementTreePath);
+  const treeWithElementAtNewElementPath = state.setIn(newElementTreePath, element);
 
   // Return tree with element at new element path, with value at old element path deleted
   return treeWithElementAtNewElementPath.deleteIn(elementPath);
--- app/InterfaceEditor/reducers/tree/reduceRemoveElement.js
+++ app/InterfaceEditor/reducers/tree/reduceRemoveElement.js
@@ -1,3 +1,7 @@
+import treePathByElementPath from '../../utils/treePathByElementPath';
+
 export default function reduceRemoveElement(state, { elementPath }) {
-  return state.deleteIn(elementPath);
+  const elementTreePath = treePathByElementPath(elementPath);
+
+  return state.deleteIn(elementTreePath);
 }
