7271e0d23a1d87a8ac8995f9e595d1405eafba65
Dan Scanlon <dscanlonpa@gmail.com>
2016-02-18 19:30:31 -0500

    Removed redux-thunk

--- app/store/middleware/index.js
+++ app/store/middleware/index.js
@@ -1,9 +1,7 @@
 import analytics from 'react-native-redux-segment';
-import thunk from 'redux-thunk';
 import logger from './logger';
 
 export default [
-  thunk,
   logger,
   analytics({ writeKey: 'bLD29v49i26Gz6LJT1dyTPX6uzCF63sr' }),
 ];
