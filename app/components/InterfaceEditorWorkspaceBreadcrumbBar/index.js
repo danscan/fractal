7b57c9796dfe2252322f80960d4286f9ff3138da
Dan Scanlon <dscanlonpa@gmail.com>
2016-03-12 00:31:25 -0500

    Added live actions to interface editor workspace breadcrumb bar

--- app/components/InterfaceEditorWorkspaceBreadcrumbBar/index.js
+++ app/components/InterfaceEditorWorkspaceBreadcrumbBar/index.js
@@ -1,11 +1,14 @@
 import { connect } from 'react-redux';
 import { interfaceEditorSelectedElementBreadcrumbElements } from '../../selectors/interfaceEditorComponents';
+import { setInterfaceEditorSelectedElementPath } from '../../actions/interfaceEditorSelectedElementPath';
 import Component from './component';
 
 const mapStateToProps = (state) => ({
   selectedElementBreadCrumbElements: interfaceEditorSelectedElementBreadcrumbElements(state),
 });
 
-const actionCreators = {};
+const actionCreators = {
+  onPressElement: setInterfaceEditorSelectedElementPath,
+};
 
 export default connect(mapStateToProps, actionCreators)(Component);
