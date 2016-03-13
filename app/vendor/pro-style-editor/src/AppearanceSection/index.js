71f01b4d683701ef426ac73d78b2de7168d8f602
Dan Scanlon <dscanlonpa@gmail.com>
2016-03-13 19:49:46 -0400

    Implemented style editor input appearance section UI

--- app/vendor/pro-style-editor/src/AppearanceSection/index.js
+++ app/vendor/pro-style-editor/src/AppearanceSection/index.js
@@ -1,5 +1,10 @@
-import React, { Component, PropTypes, View } from 'react-native';
+import React, { Component, Image, PropTypes, View } from 'react-native';
 import ImmutablePropTypes from 'react-immutable-proptypes';
+import {
+  ColorField,
+  NumberIncrementField,
+  RadioButtonsGroup,
+} from 'pro-inputs';
 import styles from './styles';
 
 export default class AppearanceSection extends Component {
@@ -14,7 +19,45 @@ export default class AppearanceSection extends Component {
 
   render() {
     return (
-      <View style={styles.continer}/>
+      <View style={styles.container}>
+        <ColorField
+          name="Background Color"
+          value="#96c400"
+        />
+
+        <RadioButtonsGroup
+          name="Backface Visibility"
+          items={[
+            {
+              image: (<Image
+                source={require('../../assets/img/radioShow.png')}
+                style={{
+                  width: 17,
+                  height: 9,
+                }}
+              />),
+              value: 'visible',
+            },
+            {
+              image: (<Image
+                source={require('../../assets/img/radioCross.png')}
+                style={{
+                  width: 12,
+                  height: 12,
+                }}
+              />),
+              value: 'hidden',
+            },
+          ]}
+          selectedValue="visible"
+        />
+
+        <NumberIncrementField
+          name="Opacity"
+          unit="%"
+          value={100}
+        />
+      </View>
     );
   }
 }
--- app/vendor/pro-style-editor/src/AppearanceSection/styles.js
+++ app/vendor/pro-style-editor/src/AppearanceSection/styles.js
@@ -2,6 +2,9 @@ import { StyleSheet } from 'react-native';
 
 export default StyleSheet.create({
   container: {
-    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    paddingLeft: 10,
+    paddingVertical: 10,
   },
 });
