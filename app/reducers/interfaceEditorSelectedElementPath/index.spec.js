1ee1b37c91a13d8dd87a8645928687ac2d8b41db
Dan Scanlon <dscanlonpa@gmail.com>
2016-03-20 16:48:18 -0400

    Added test for REMOVE_INTERFACE_EDITOR_COMPONENT_ELEMENT action type

--- app/reducers/interfaceEditorSelectedElementPath/index.spec.js
+++ app/reducers/interfaceEditorSelectedElementPath/index.spec.js
@@ -1,9 +1,9 @@
 /* global it, describe */
 import expect from 'expect';
+import { List } from 'immutable';
 import reducer, { initialState } from './';
-import {
-  setInterfaceEditorSelectedElementPath,
-} from '../../actions/interfaceEditorSelectedElementPath';
+import { setInterfaceEditorSelectedElementPath } from '../../actions/interfaceEditorSelectedElementPath';
+import { removeInterfaceEditorComponentElement } from '../../actions/interfaceEditorComponents';
 
 describe('interfaceEditorSelectedElementPath reducer', () => {
   it('should return the initial state', () => {
@@ -18,5 +18,14 @@ describe('interfaceEditorSelectedElementPath reducer', () => {
     expect(
       reducer(undefined, setInterfaceEditorSelectedElementPath(selectedElementPath))
     ).toEqual(selectedElementPath);
+  });
+
+  it('should handle action created by the removeInterfaceEditorComponentElement action creator', () => {
+    const selectedElementPath = new List([0, 1]);
+    const expectedNewSelectedElementPath = selectedElementPath.pop();
+
+    expect(
+      reducer(selectedElementPath, removeInterfaceEditorComponentElement(selectedElementPath))
+    ).toEqual(expectedNewSelectedElementPath);
   });
 });
