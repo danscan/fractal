9dc4161ecbc815a72063590519390af5bd89ecaf
Dan Scanlon <dscanlonpa@gmail.com>
2016-03-20 16:42:24 -0400

    Prevent selected element that does not exist when user removes selected element

--- app/reducers/interfaceEditorSelectedElementPath/index.js
+++ app/reducers/interfaceEditorSelectedElementPath/index.js
@@ -1,3 +1,4 @@
+import { REMOVE_INTERFACE_EDITOR_COMPONENT_ELEMENT } from '../../actions/interfaceEditorComponents';
 import { SET_INTERFACE_EDITOR_SELECTED_ELEMENT_PATH } from '../../actions/interfaceEditorSelectedElementPath';
 import { List } from 'immutable';
 
@@ -7,6 +8,14 @@ export default function interfaceEditorSelectedElementPath(state = initialState,
   switch (action.type) {
     case SET_INTERFACE_EDITOR_SELECTED_ELEMENT_PATH:
       return action.selectedElementPath;
+
+    // When user removes the selected element, select its parent (so to avoid
+    // having a selected element that no longer exists)...
+    case REMOVE_INTERFACE_EDITOR_COMPONENT_ELEMENT:
+      return state.equals(action.elementPath)
+        ? action.elementPath.pop()
+        : state;
+
     default:
       return state;
   }
