7888bb41d9d5b7637091a35c867f4305ce2a174b
Dan Scanlon <dscanlonpa@gmail.com>
2016-03-28 09:43:28 -0400

    Updated onPressElementType implementation to remove selectedComponentKey from action creator invocation

--- app/components/InterfaceEditorAddElementPanel/index.js
+++ app/components/InterfaceEditorAddElementPanel/index.js
@@ -1,14 +1,12 @@
 import { connect } from 'react-redux';
 import { addInterfaceEditorComponentElementChild } from '../../actions/interfaceEditorComponent';
 import elementTypeGroups from '../../constants/elementTypes';
-import { interfaceEditorSelectedComponentKey } from '../../selectors/interfaceEditorSelectedComponentKey';
 import { interfaceEditorSelectedElementPath } from '../../selectors/interfaceEditorSelectedElementPath';
 import elementByType from '../../utils/elementByType';
 import Component from './component';
 
 const mapStateToProps = (state) => ({
   elementTypeGroups,
-  selectedComponentKey: interfaceEditorSelectedComponentKey(state),
   selectedElementPath: interfaceEditorSelectedElementPath(state),
 });
 
@@ -18,7 +16,6 @@ const actionCreators = {
 
 const mergeProps = (stateProps, dispatchProps, ownProps) => {
   const onPressElementType = (elementType) => dispatchProps.onPressElementType(
-    stateProps.selectedComponentKey,
     stateProps.selectedElementPath,
     elementByType(elementType.get('component')),
   );
