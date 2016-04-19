ee35739c6eee84279a676649c0b0ec99de3edd12
Dan Scanlon <dscanlonpa@gmail.com>
2016-04-19 11:42:23 -0400

    Removed unused interfaceEditorSelectedComponentKey state domain

--- app/actions/interfaceEditorSelectedComponentKey.js
+++
@@ -1,8 +0,0 @@
-export const SET_INTERFACE_EDITOR_SELECTED_COMPONENT_KEY = 'SET_INTERFACE_EDITOR_SELECTED_COMPONENT_KEY';
-
-export function setInterfaceEditorSelectedComponentKey(selectedComponentKey) {
-  return {
-    type: SET_INTERFACE_EDITOR_SELECTED_COMPONENT_KEY,
-    selectedComponentKey,
-  };
-}
