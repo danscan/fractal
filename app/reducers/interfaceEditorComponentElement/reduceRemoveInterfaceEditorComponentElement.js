a609135ad1e716b1a8c8d4602a8be976482f9ee1
Dan Scanlon <dscanlonpa@gmail.com>
2016-05-10 12:27:40 -0400

    Removed a log

--- app/reducers/interfaceEditorComponentElement/reduceRemoveInterfaceEditorComponentElement.js
+++ app/reducers/interfaceEditorComponentElement/reduceRemoveInterfaceEditorComponentElement.js
@@ -2,7 +2,6 @@ import treePathByElementPath from '../../utils/treePathByElementPath';
 import emptyRootElement from '../../constants/emptyRootElement';
 
 export default function reduceRemoveInterfaceEditorComponentElement(state, { elementPath }) {
-  console.log('state:', state);
   const elementTreePath = treePathByElementPath(elementPath);
 
   // Avoid result in which tree has no root element. If element path is root,
